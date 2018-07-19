a64ce435ccafaf2eb745166359008630b5665c1d
duduzhu <duduzhu@gmail.com>
2018-07-19 21:39:36 +0800

    callback的添加是有顺序的，消息被前面的handler捕捉就不会向后传递。修正了被commandhandler捕捉走新成员消息的bug

--- startBot.py
+++ startBot.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+import json
 
 from telegram import KeyboardButton, ParseMode, ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
@@ -14,7 +15,15 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 BLACKLIST= set()
+
 BNB48=-1001136778297
+SPAMWORDS=["Deleted", u"拉人",u"电报群",u"水军",u"增粉"];
+SirIanM=420909210
+Gui=434121211
+coinrumorbot=405689392
+bnb48_bot=571331274
+NOTIFYADMINS="有新成员加入，需要审批:) "
+
 
 def help(bot, update):
     """Send a message when the command /help is issued."""
@@ -22,32 +31,48 @@ def help(bot, update):
 
 
 def botcommandhandler(bot, update):
-    """get message not mentioned and buttons response."""
-
-    '''check if is button response'''
     message_text = update.message.text
-    response = menu[message_text]
-    bot.sendMessage(update.message.chat_id, text=response, reply_to_message_id=update.message.message_id,parse_mode=ParseMode.MARKDOWN)
+    if "#SellBNBAt48BTC" in message_text:
+        logger.warning('botcommandhandler')
+        file=open("/var/www/html/sell48","r")
+        content = json.load(file)
+        response="目前48BTC挂单量为{}BNB".format(content['amt'])
+        bot.sendMessage(update.message.chat_id, text=response, reply_to_message_id=update.message.message_id,parse_mode=ParseMode.MARKDOWN)
+        file.close()
 
 def replyCommand(bot,update):
+    # Only take care of replys in BNB48
     if update.message.chat_id != BNB48:
         logger.warning('not this group')
         return
-
-    replyed = update.message.reply_to_message
-    beingreplieduser = update.message.reply_to_message.from_user
-
-    if beingreplieduser.id != 571331274 and beingreplieduser.id != 405689392:
-        logger.warning('not to me, to {}'.format(beingreplieduser.id))
-        return
-
+    # Only admins can reply
     talkingmember = bot.getChatMember(BNB48, update.effective_user.id)
     if talkingmember.status != 'creator' and talkingmember.status != 'administrator':
         #bot.sendMessage(update.message.chat_id, text="不是管理员不要捣蛋", reply_to_message_id=update.message.message_id,parse_mode=ParseMode.MARKDOWN)
         logger.warning(talkingmember.status)
         return
-    newmember = replyed.forward_from
-    newmemberid = replyed.forward_from.id
+
+    # If directly reply the newmember msg
+    replyed = update.message.reply_to_message
+    # If message == pass && new members
+    if hasattr(replyed,'new_chat_members') and len(replyed.new_chat_members)>0 and update.message.text == 'pass':
+        for newUser in replyed.new_chat_members:
+            bot.restrictChatMember(update.message.chat_id,user_id=newUser.id,can_send_messages=True,can_send_media_messages=True,can_send_other_messages=True, can_add_web_page_previews=True)
+            logger.warning(newUser.full_name+" passed");
+        return
+
+    # If a normal forwarded pass process
+    beingreplieduser = replyed.from_user
+    if beingreplieduser.id != 571331274 and beingreplieduser.id != 405689392:
+        logger.warning('not to me, to {}'.format(beingreplieduser.id))
+        return
+
+    try:
+        newmember = replyed.forward_from
+        newmemberid = replyed.forward_from.id
+    except AttributeError:
+        logger.warning('not a forward message')
+        return
 
     if update.message.text == 'pass':
         newchatmember = bot.getChatMember(BNB48, newmemberid)
@@ -76,15 +101,25 @@ def photoHandler(bot,update):
 
     chatmember = bot.getChatMember(BNB48,userid)
     logger.warning(chatmember.can_send_messages)
-    if None != chatmember.can_send_messages and True != chatmember.can_send_messages:
+    sayingmember = bot.getChatMember(BNB48, userid)
+    if sayingmember.status == 'restricted' or userid == SirIanM:
         forward = bot.forwardMessage(BNB48,update.effective_user.id,update.message.message_id)
         bot.sendMessage(update.message.chat_id, text=u"已提交持仓证明，请关注群内审批情况，耐心等待。如无必要，无需频繁重复发送。", reply_to_message_id=update.message.message_id,parse_mode=ParseMode.MARKDOWN)
-        #bot.sendMessage(BNB48, text=NOTIFYADMINS, reply_to_message_id=forward.message_id)
+        #给每名管理员私聊发送提醒
+        admins = bot.getChatAdministrators(BNB48)
+        for eachadmin in admins:
+            try:
+                bot.sendMessage(eachadmin.user.id, text=NOTIFYADMINS,parse_mode ="Markdown")
+            except TelegramError:
+                print('TelegramError, could be while send private message to admins')
+                continue
 
     
 def onleft(bot,update):
-    if u"拉人" in update.message.left_chat_member.full_name or "Deleted Account" in update.message.left_chat_member.full_name :
-        bot.deleteMessage(update.message.chat_id,update.message.message_id)
+    for SPAMWORD in SPAMWORDS:
+        if SPAMWORD in update.message.left_chat_member.full_name:
+            bot.deleteMessage(update.message.chat_id,update.message.message_id)
+            return
 
 def welcome(bot, update):
     '''
@@ -98,9 +133,10 @@ def welcome(bot, update):
     #首先筛选垃圾消息
     isSpam = False
     for newUser in update.message.new_chat_members:
-        if u"拉人" in update.message.left_chat_member.full_name or "Deleted Account" in update.message.left_chat_member.full_name :
-            isSpam = True
-            break;
+        for SPAMWORD in SPAMWORDS:
+            if SPAMWORD in newUser.full_name:
+                isSpam = True
+                break;
 
     if isSpam:
         bot.deleteMessage(update.message.chat_id,update.message.message_id)
@@ -109,7 +145,7 @@ def welcome(bot, update):
             logger.warning('%s|%s is kicked because of spam',newUser.id,newUser.full_name)
             
     if update.message.chat_id == BNB48:
-        bot.sendMessage(update.message.chat_id, text=u"欢迎。新成员默认禁言，请私聊 [BNB48 - 静静](tg://user?id=571331274)  发送#SellBNBAt48BTC 的挂单截图(500BNB或以上)，审核通过后开启权限成为正式会员。持仓截图会被机器人自动转发进群，请注意保护个人隐私。", reply_to_message_id=update.message.message_id,parse_mode=ParseMode.MARKDOWN)
+        bot.sendMessage(update.message.chat_id, text=u"欢迎。新成员默认禁言，请私聊 [BNB48 - 静静](tg://user?id=571331274)  发送#SellBNBAt48BTC 的挂单截图(500BNB或以上，Photo形式，别发File形式)，审核通过后开启权限成为正式会员。持仓截图会被机器人自动转发进群，请注意保护个人隐私。", reply_to_message_id=update.message.message_id,parse_mode=ParseMode.MARKDOWN)
         for newUser in update.message.new_chat_members:
             bot.restrictChatMember(update.message.chat_id,user_id=newUser.id, can_send_messages=False)
 
@@ -132,11 +168,12 @@ def main():
     #dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.reply & Filters.group, replyCommand))# '''处理大群中的回复'''
-    dp.add_handler(MessageHandler(Filters.text and Filters.private, callback=botcommandhandler))#'''处理私聊文字'''
+    #dp.add_handler(MessageHandler(Filters.text and Filters.private, callback=botcommandhandler))#'''处理私聊文字'''
     dp.add_handler(MessageHandler(Filters.photo & Filters.private, callback=photoHandler))#'''处理私发的图片'''
     dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, welcome))#'''处理新成员加入'''
     dp.add_handler(MessageHandler(Filters.status_update.left_chat_member, onleft))#'''处理成员离开'''
+    dp.add_handler(MessageHandler(Filters.group & Filters.reply, replyCommand))# '''处理大群中的回复'''
+    dp.add_handler(MessageHandler(Filters.group & (~Filters.reply),botcommandhandler))# '''处理大群中的普通消息'''
 
     # log all errors
     dp.add_error_handler(error)
