15b32311602d71df51ba8bd2493a759b2f2d0320
duduzhu <duduzhu@gmail.com>
2018-11-10 00:30:44 +0800

    should use re.search not match; match wants a perfect match

--- community.py
+++ community.py
@@ -319,8 +319,8 @@ def forwardHandler(bot,update):
     suspectScam = False
     if globalconfig.has_section("scamkeys"):
         for scamkey in globalconfig.items("scamkeys"):
-            if not re.match(scamkey[0],fwduser.username,re.IGNORECASE) is None or not re.match(scamkey[0],fwduser.full_name,re.IGNORECASE) is None:
-                logger.warning("Hit scam key {}".format(scamkey))
+            if not re.search(scamkey[0],fwduser.username,re.IGNORECASE) is None or not re.search(scamkey[0],fwduser.full_name,re.IGNORECASE) is None:
+                logger.warning("{}/{} Hit scam key {}".format(fwduser.username,fwduser.full_name,scamkey))
                 suspectScam = True
                 break
 
